4f4cf5ca4fa07e50628abeaf1c2c3024cb2fa9f2
Mahrjose <mirzamahrabhossain@gmail.com>
2022-02-15 19:16:27 +0600

    "Entry Point" for the discord bot

---
+++ bot.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import os
+import discord
+from discord.ext import commands
+
+from dotenv import load_dotenv
+import ping
+
+
+class CustomBotClient(commands.Bot):
+    def __init__(self, command_prefix, **options):
+        super().__init__(command_prefix, **options)
+        self.load_cogs()
+
+    def load_cogs(self):
+        for filename in os.listdir("cogs"):
+
+            if filename.endswith(".py"):
+                extension = f"cogs.{filename[:-3]}"
+                self.load_extension(extension)
+                print(f"Extension {extension} Loaded Successfully.")
+
+    async def on_ready(self):
+        await self.change_presence(
+            status=discord.Status.online, activity=discord.Game("Game of Life")
+        )
+        print(f"Bot {self.user} is connected to Discord and ready to roll!")
+
+
+def main():
+    bot = CustomBotClient(command_prefix="-")
+    ping.alive()
+
+    load_dotenv()
+    bot.run(os.getenv("TOKEN"))
+
+
+if __name__ == "__main__":
+    main()
