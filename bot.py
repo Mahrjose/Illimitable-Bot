aa9d4e6c1109207041614e928129c860454e2925
Mahrjose <mirzamahrabhossain@gmail.com>
2022-03-02 05:32:44 +0600

    Implement Logging

--- bot.py
+++ bot.py
@@ -6,6 +6,17 @@ from discord.ext import commands
 
 from dotenv import load_dotenv
 import server
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def setup_logger() -> None:
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    file_handler = logging.FileHandler("Logs/Bot.log")
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
 
 class IllimitableBot(commands.Bot):
@@ -20,18 +31,23 @@ class IllimitableBot(commands.Bot):
             if filename.endswith(".py"):
                 extension = f"cogs.{filename[:-3]}"
                 self.load_extension(extension)
+                logger.debug(f"Extension {extension} Loaded Successfully")
                 print(f"Extension {extension} Loaded Successfully.")
 
     async def on_ready(self):
         await self.change_presence(
             status=discord.Status.online, activity=discord.Game("Game of Life")
         )
+        logger.debug(f"Bot {self.user} is connected to Discord and ready to roll!")
         print(f"Bot {self.user} is connected to Discord and ready to roll!")
 
 
 def main():
+    setup_logger()
+    logger.debug("Starting Illimitable-Bot.....")
     bot = IllimitableBot(command_prefix=".")
-    server.alive()
+    logger.debug("--------------------------------------------")
+    # server.alive()
 
     load_dotenv()
     bot.run(os.getenv("TOKEN"))
