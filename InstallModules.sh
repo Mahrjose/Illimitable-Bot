dde27cef3f12f94b8c10c6b406a8fb9817195909
Mahrjose <mirzamahrabhossain@gmail.com>
2022-03-01 17:48:24 +0600

    Add a shell script for Bulk-Installing necessary modules

---
+++ InstallModules.sh
@@ -0,0 +1,33 @@
+# Run this after creating virtual environment for installing all the necessary modules.
+
+# Install logger
+pip install logger
+
+# Install Formatter Black
+pip install black
+
+# Install Pycord
+pip install py-cord
+
+# Install Dot-Env (for loading .env files)
+pip install python-dotenv
+
+# Install Flask
+pip install flask
+
+### For Cogs.News
+# install Feed Parser (for parsing RSS feed)
+pip install feedparser
+
+# install requests
+pip install requests
+
+# install BeautifulSoup
+pip install bs4
+
+### For Cogs.search
+# Install Wikipedia
+pip install wikipedia
+
+# Install Wikipedia-Api
+pip install wikipedia-api
